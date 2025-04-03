b77a2d29ee0c1726160aa217ec3a79c1eedc89df
GiuliaSouzaDaF <giuliasouzadafonsecasc@gmail.com>
2025-04-02 21:19:03 -0300

    mexemos no css e botamos icons

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { DiGithubBadge } from "react-icons/di";
+function App() {
+  
+
+  return (
+    <div className='container-app'>
+      <h1>Links da Giu </h1>
+
+      <a href="https://github.com/GiuliaSouzaDaF" className='links' target='_blank'><DiGithubBadge />
+      
+      Github</a>
+      
+    </div>
+  )
+}
+
+export default App
